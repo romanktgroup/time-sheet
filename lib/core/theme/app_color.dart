d6f88262eae1af7eb5905d5a50383f55f04552f3
Nikolay Kantur <nikolay.kantur@gmail.com>
2024-08-18 11:55:17 +0300

    basic structure

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const background = Color(0xFFEFF0F7);
+  static const titleMonth = Color(0xFF1C1D28);
+  static const backgroundPurple = Color(0xFFE4DBFF);
+  static const titleDay = Color(0xFF565656);
+  static const titleDayOther = Color(0xFFB7B7B7);
+  static const blue = Color(0xFF4807FF);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const green = Color(0xFF2EE180);
+}
---
+++ lib/core/theme/app_style.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyle {
+  static const inter14w500 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14,
+    height: 16.94 / 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const inter14w400 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14,
+    height: 16.94 / 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const inter16w500 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16,
+    height: 19.36 / 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const inter18w500 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18,
+    height: 21.78 / 18,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const inter20w400 = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20,
+    height: 28 / 20,
+    fontWeight: FontWeight.w400,
+  );
+}
